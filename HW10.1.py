39b854d31b3ab282349c5f4fc8aa65fdf35c6421
Denchai R <41133472+DenchaiR@users.noreply.github.com>
2024-05-31 09:00:55 +0700

    เพิ่มดูประวัติ
    สามารถดูประวัติการคำนวณย้อนหลังได้

--- HW10.1.py
+++ HW10.1.py
@@ -2,6 +2,11 @@ import ttkbootstrap as ttk  # ใช้ ttkbootstrap แทน ttk
 from ttkbootstrap.constants import *
 from tkinter import *
 import csv
+import os
+
+path = os.getcwd()
+
+noteicon = os.path.join(path, 'note_icon.ico')
 
 # โปรแกรมสำหรับคำนวณยอดผ่อนบัตรเครดิตต่อเดือนโดยที่จ่ายเต็มจำนวนทุกเดือน
 def cal_credit_interest(price, month, interest=0.0069):
@@ -16,19 +21,7 @@ def cal_credit_interest(price, month, interest=0.0069):
     )
     return result
 
-def calculate():
-    try:
-        price = float(E2.get())
-        month = int(E3.get())
-        result = cal_credit_interest(price, month)
-        result_label.delete(1.0, END)  # ลบข้อมูลทั้งหมดที่อยู่ใน Text widget
-        result_label.insert(END, result)  # เพิ่มข้อความใหม่เข้าไปใน Text widget
-        
-    except ValueError:
-        result_label.delete(1.0, END)  # ลบข้อมูลทั้งหมดที่อยู่ใน Text widget
-        result_label.insert(END, "กรุณากรอกข้อมูลให้ถูกต้อง")
-
-def save():
+def calculate_and_save():
     try:
         price = float(E2.get())
         month = int(E3.get())
@@ -39,9 +32,10 @@ def save():
             writer = csv.writer(file)
             writer.writerow([price, month, result])
         
-        result_label.delete(1.0, END)
-        result_label.insert(END, "ข้อมูลถูกบันทึกลงในไฟล์ credit_payment.csv เรียบร้อยแล้ว")
-
+        result_label.delete(1.0, END)  # ลบข้อมูลทั้งหมดที่อยู่ใน Text widget
+        result_label.insert(END, result)  # เพิ่มข้อความใหม่เข้าไปใน Text widget
+        result_label.insert(END, "\n\nข้อมูลถูกบันทึกลงในไฟล์ credit_payment.csv เรียบร้อยแล้ว")
+        
         # แสดงข้อมูลใน Terminal
         print(f'จำนวนเงินที่ต้องการผ่อน: {price} บาท')
         print(f'จำนวนเดือนที่ต้องการผ่อน: {month} เดือน')
@@ -56,7 +50,8 @@ def save():
 # หน้าต่างของการใช้งาน GUI
 GUI = ttk.Window(themename='darkly')  # สร้างหน้าต่าง GUI พร้อมกำหนดธีม
 GUI.title('โปรแกรมคำนวณยอดผ่อนบัตรเครดิต By Denchai R.')
-GUI.geometry('300x700')
+GUI.geometry('300x720')
+GUI.iconbitmap(noteicon)  # ตั้งค่าไอคอนที่นี่
 
 # Build main label
 L1 = ttk.Label(GUI, text='โปรแกรมคำนวณยอดผ่อนบัตรเครดิต', font=('Angsana New', 20))
@@ -64,6 +59,77 @@ L1.pack()
 L1_1 = ttk.Label(GUI, text='By Denchai R.', font=('Angsana New', 20))
 L1_1.pack()
 L1_1.pack(pady=10)
+
+# Build sub label 4
+L4 = ttk.Label(GUI, text='ดูประวัติ', font=('Angsana New', 18))
+L4.pack(pady=2)
+
+####################BUTTON FLASHCARD#############################
+def readcsv():
+    with open('credit_payment.csv', newline='', encoding='utf-8') as file:
+        fr = csv.reader(file)  # fw = file read
+        data = list(fr)
+        return data
+
+flashcard_list = readcsv()
+
+global countindex
+countindex = 0
+
+def Flashcard():
+    flashcard_list = readcsv()
+
+    GUI2 = Toplevel()
+    GUI2.title('ประวัติ')
+    GUI2.geometry('500x400')
+
+    vtext_title = StringVar()
+    vtext_subtitle = StringVar()
+    vtext_detail = StringVar()
+    title = ttk.Label(GUI2, textvariable=vtext_title, font=('Angsana New', 20, 'bold'))
+    title.pack()
+    vtext_title.set(flashcard_list[0][0])
+    subtitle = ttk.Label(GUI2, textvariable=vtext_subtitle, font=('Angsana New', 20, 'bold'))
+    subtitle.pack()
+    vtext_subtitle.set(flashcard_list[0][1].replace('\r', ''))
+    detail = ttk.Label(GUI2, textvariable=vtext_detail, font=('Angsana New', 18))
+    detail.pack()
+    vtext_detail.set(flashcard_list[0][2].replace('\r', ''))
+
+    def Prev():
+        global countindex
+        if countindex == 0:
+            countindex = countindex
+        else:
+            countindex = countindex - 1
+        # set text
+        vtext_title.set(flashcard_list[countindex][0])
+        vtext_subtitle.set(flashcard_list[countindex][1].replace('\r', ''))
+        vtext_detail.set(flashcard_list[countindex][2].replace('\r', ''))
+
+    BPrev = ttk.Button(GUI2, text='<', command=Prev)
+    BPrev.place(x=210, y=350)
+
+    def Next():
+        global countindex
+        if countindex == (len(flashcard_list) - 1):
+            countindex = len(flashcard_list) - 1
+        else:
+            countindex = countindex + 1
+        # set text
+        vtext_title.set(flashcard_list[countindex][0])
+        vtext_subtitle.set(flashcard_list[countindex][1].replace('\r', ''))
+        vtext_detail.set(flashcard_list[countindex][2].replace('\r', ''))
+    
+    BNext = ttk.Button(GUI2, text='>', command=Next)
+    BNext.place(x=260, y=350)
+
+    GUI2.mainloop()
+
+notebutton = os.path.join(path, 'note_png_48.png')
+notebutton = PhotoImage(file=notebutton)
+BFlashcard = ttk.Button(GUI, image=notebutton, command=Flashcard)             
+BFlashcard.pack()
 
 # Build sub label 1
 L2 = ttk.Label(GUI, text='จำนวนเงินที่ต้องการผ่อน', font=('Angsana New', 18))
@@ -82,19 +148,15 @@ E3 = ttk.Entry(GUI, font=('Angsana New', 18), width=30)
 E3.pack(pady=2)
 
 # Build button with custom style
-B1 = ttk.Button(GUI, text='คำนวณ', style='TButton', command=calculate)
+B1 = ttk.Button(GUI, text='คำนวณ', style='TButton', command=calculate_and_save)
 B1.pack(pady=10, ipadx=20, ipady=10)
-
-# Add save button
-B2 = ttk.Button(GUI, text='บันทึก', style='TButton', command=save)
-B2.pack(pady=10, ipadx=20, ipady=10)
 
 # Build result label
 L4 = ttk.Label(GUI, text='ผลลัพธ์', font=('Angsana New', 18))
 L4.pack(pady=2)
 
 # Build result box 
-result_label = Text(GUI, font=('Angsana New', 18), wrap=WORD, height=8, width=30)
+result_label = Text(GUI, font=('Angsana New', 18), wrap=WORD, height=6, width=30)
 result_label.pack(pady=2)
 
 GUI.mainloop()
